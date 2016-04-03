7022a3581f45235f149c10d31795c057ab42ed66
Tania Shatilova <tatashat@gmail.com>
2016-04-03 19:55:39 +0300

    + Custom Burn UI with options (pure WPF)

--- CopyBinaries/README.md
+++ CopyBinaries/README.md
@@ -1,5 +1,7 @@
-# Windows Installer XML (WiX) Samples
+CopyBinaries
 
-## Samples
+## Description
+This [WiX](http://wixtoolset.org/) installer copies binaries to setup folder. In this solution there is a library which I want to copy in setup directory without referencing this lib in setup project. To find out more about setup project go to [ReadMe.txt](https://github.com/Sufflavus/WixExamples/blob/master/CopyBinaries/Source/Setup/ReadMe.txt).
 
-### [Copy Binaries](CopyBinaries)
+## List of used tecnologies and libraries:
+1. [WiX v3.10.2](http://wixtoolset.org/) 
---
+++ CustomBurnUiWithOptionsOnPureWpf/Source/CustomBA/Views/InstallView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using CustomBA.ViewModels;
+
+namespace CustomBA.Views
+{
+    /// <summary>
+    ///     Interaction logic for InstallView.xaml
+    /// </summary>
+    public partial class InstallView : Window
+    {
+        public InstallView(InstallViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+
+            // This will be called if the user closes the window instead of clicking on the Cancel button. 
+            // Our method guides the code to execute our CancelCommand.
+            Closed += (sender, e) => viewModel.CancelCommand.Execute(this);
+        }
+    }
+}
